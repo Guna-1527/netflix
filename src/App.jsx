a580de6c83a94be01ee745ef4b9c48ebf0b53b2e
Guna-1527 <126180469+Guna-1527@users.noreply.github.com>
2023-11-25 13:49:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Movie from "./components/Movie/Movie"
 import "./App.css"
 import SignIn from "./Pages/SignIn/SignIn"
+import Home from "./Pages/Home"
 import MoviePlayer from "./components/MoviePlayer/MoviePlayer"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Movie />} />
         <Route path="/:id/:title" element={<MoviePlayer />} />
       </Routes>
